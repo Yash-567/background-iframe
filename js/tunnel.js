72ec1651688494c988f00079d8203c8f48779f88
Yash Sonar <46718837+Yash-567@users.noreply.github.com>
2020-12-24 23:50:14 +0530

    Blue tunnel

--- js/tunnel.js
+++ js/tunnel.js
@@ -229,7 +229,7 @@ window.onload = function() {
   loader.crossOrigin = "Anonymous";
 
   loader.load(
-    "image/galaxytexture.jpg",
+    "image/2454628.jpg",
     function(texture) {
       document.body.classList.remove("loading");
       window.tunnel = new Tunnel(texture);
